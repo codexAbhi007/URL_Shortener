b4186261bfac577b48473a1060c4667571f541d2
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-06-09 11:57:27 +0530

    add frontend after email generation

--- frontend/src/pages/auth/Profile.jsx
+++ frontend/src/pages/auth/Profile.jsx
@@ -3,9 +3,13 @@ import { getProfile, logoutUser } from "../../api/axios_api";
 import Context from "../../Context";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-
+import { MdModeEdit } from "react-icons/md";
+import { IoKeySharp } from "react-icons/io5";
+import { CgLogOut } from "react-icons/cg";
+import { IoMail } from "react-icons/io5";
 const Profile = () => {
   const [user, setUser] = useState(null);
+
   const { setIsAuthenticated, setUser: setGlobalUser } = useContext(Context);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
@@ -14,15 +18,15 @@ const Profile = () => {
     const fetchProfile = async () => {
       try {
         const res = await getProfile();
-        console.log(res);
         if (res.status === 200) {
           setUser(res.data.user);
+          console.log(res.data)
           toast.success("Welcome to Profile Page", { id: "profile-toast" });
         }
-        // eslint-disable-next-line no-unused-vars
       } catch (err) {
         toast.error("Please Login to Access", { id: "auth-error" });
         navigate("/app/login");
+        console.log(err);
       } finally {
         setLoading(false);
       }
@@ -54,31 +58,56 @@ const Profile = () => {
   }
 
   return (
-    <div className="grid place-items-center min-h-[70vh] px-4">
-      <div className="w-full max-w-md bg-white shadow-xl rounded-lg p-6 border border-gray-200">
-        <div className="flex items-center justify-between mb-6">
-          <h2 className="text-2xl font-bold text-gray-800">👤 Your Profile</h2>
-          <button
-            onClick={handleLogout}
-            className="bg-red-500 hover:bg-red-900 hover:cursor-pointer text-white text-sm px-4 py-2 rounded transition"
-          >
-            Logout
-          </button>
+    <div className="flex flex-col items-center bg-gray-100 px-4 py-10">
+      <h1 className="text-3xl font-semibold text-center mb-4">Your Profile</h1>
+
+      <div className="bg-white shadow-xl hover:shadow-2xl transition-shadow duration-300 rounded-lg p-6 max-w-md w-full text-center ring-1 ring-gray-200">
+        <div className="rounded-full bg-blue-600 text-white w-16 h-16 mx-auto text-2xl flex items-center justify-center font-bold uppercase mb-4">
+          {user.username[0]}
         </div>
 
-        <div className="space-y-4">
+        <h2 className="text-xl font-bold text-gray-800 mb-1">
+          {user.username}
+        </h2>
+        <div>{user.verified == false ? "Not verified" : "Verified"}</div>
+        <div className="flex w-full items-center justify-center gap-2">
+          <IoMail size={18} />
+          <p className="text-gray-600">{user.email}</p>
+        </div>
+
+        <p className="text-gray-500 text-sm mb-4">
+          Member since {new Date(user.createdAt).toLocaleDateString()}
+        </p>
+
+        <div className="grid grid-cols-3 gap-4 text-sm text-gray-700 mt-6 mb-4">
           <div>
-            <p className="text-gray-600 text-sm">Username</p>
-            <p className="text-lg font-medium text-gray-900">{user.username}</p>
+            <p className="font-bold text-lg">{user.links || 0}</p>
+            <p>Links Created</p>
           </div>
           <div>
-            <p className="text-gray-600 text-sm">Email</p>
-            <p className="text-lg font-medium text-gray-900">{user.email}</p>
+            <p className="font-bold text-lg">0</p>
+            <p>Total Clicks</p>
           </div>
           <div>
-            <p className="text-gray-600 text-sm">User ID</p>
-            <p className="text-lg font-medium text-gray-900">{user.id}</p>
+            <p className="font-bold text-lg">Today</p>
+            <p>Last Active</p>
           </div>
+        </div>
+
+        <div className="flex flex-col gap-3 mt-4">
+          <button className="bg-blue-600 flex items-center justify-center gap-2 text-white py-2 px-4 rounded hover:bg-blue-800 hover:cursor-pointer hover:scale-101 hover:shadow-lg transition-all duration-300 ease-in-out">
+            <MdModeEdit size={25} />
+            Edit Profile
+          </button>
+          <button className="bg-yellow-500 flex items-center justify-center gap-2 text-white py-2 px-4 rounded hover:bg-yellow-600 hover:cursor-pointer hover:scale-101 hover:shadow-lg transition-all duration-300 ease-in-out">
+            <IoKeySharp size={25} /> Change Password
+          </button>
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 flex items-center justify-center gap-2 text-white py-2 px-4 rounded hover:bg-red-700 hover:scale-101 hover:shadow-lg transition-all duration-300 ease-in-out"
+          >
+            <CgLogOut size={25} /> Logout
+          </button>
         </div>
       </div>
     </div>
